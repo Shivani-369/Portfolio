bc2b373dc6d248159e773bc0fd8336886cc92bc4
Suja Shivani S A <shivani63124@gmail.com>
2026-08-01 11:38:15 +0530

    Fix pathing in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repoName = "Portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "/Portfolio",
-  assetPrefix: "/Portfolio/",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
