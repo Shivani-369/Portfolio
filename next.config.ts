06c690e31f8da10cc252750dc2a00006f74c5612
Suja Shivani S A <shivani63124@gmail.com>
2026-08-01 11:39:21 +0530

    Set explicit basePath in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repoName = "Portfolio";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isGithubActions ? `/${repoName}` : "",
-  assetPrefix: isGithubActions ? `/${repoName}/` : undefined,
+  basePath: "/Portfolio",
 };
 
 export default nextConfig;
